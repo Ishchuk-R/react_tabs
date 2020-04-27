130adf0e8c0520312d5a1a55849ce26625dec81e
Tetiana <ishchuktr@gmail.com>
2020-04-27 17:13:14 +0300

    Updates

---
+++ precache-manifest.b9b28842207343a2b3f3c32dd32cb3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6495ff02857a2485358dbbd2bef21b2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c98ee20aab2b161a37be",
+    "url": "./static/css/main.f538ff2a.chunk.css"
+  },
+  {
+    "revision": "603417c9aa7955d00e74",
+    "url": "./static/js/2.83f88f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.83f88f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98ee20aab2b161a37be",
+    "url": "./static/js/main.e506a9e0.chunk.js"
+  },
+  {
+    "revision": "c37a9f69fe7c849220aa",
+    "url": "./static/js/runtime-main.4306ab62.js"
+  }
+]);
